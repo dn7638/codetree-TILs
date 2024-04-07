606617a74c10ffaba450da0a47e74b2979a6c564
WooHyeong Choi <48896148+dn7638@users.noreply.github.com>
2024-04-07 20:55:37 +0900

    Title: 메이즈 러너, Time: 138ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240407/메이즈 러너/maze-runner.py
+++ 240407/메이즈 러너/maze-runner.py
@@ -35,12 +35,6 @@
 
 import sys
 
-
-def Print(a):
-    for i in a:
-        print(i)
-
-
 # 입력값, 변수
 N, M, K = map(int, sys.stdin.readline().split(' '))
 graph = [[0] for _ in range(N + 1)]
@@ -57,16 +51,29 @@ for i in range(N + 1):
         continue
     graph[i].extend(list(map(int, sys.stdin.readline().split(' '))))
 graph_temp = [[0 for _ in range(N + 1)] for _ in range(N + 1)]
-# 디버깅
-# Print(graph)
-# print(player_exist)
 
 for _ in range(M):
     players.append(list(map(int, sys.stdin.readline().split(' '))))
 
 exit_location = list(map(int, sys.stdin.readline().split(' ')))
-# 디버깅
-# Print(players)
+
+def Print(a):
+    for i in range(1, len(a)):
+        for j in range(1, len(a[0])):
+            if i == exit_location[0] and j == exit_location[1]:
+                print('E', end='')
+                continue
+
+            flag = True
+            for player in players:
+                if player[0] == i and player[1] == j:
+                    print('P', end='')
+                    flag = False
+                    break
+            if not flag:
+                continue
+            print(a[i][j], end='')
+        print()
 
 
 def player_move(i):  # i = 플레이어 넘버
@@ -80,6 +87,7 @@ def player_move(i):  # i = 플레이어 넘버
             players[i][0], players[i][1] = next_x, next_y
             cnt[0] += 1
             if (next_x, next_y) == (exit_location[0], exit_location[1]):
+                players[i][0], players[i][1] = 0,0
                 player_exist[i] = False
                 number_of_players[0] -= 1
             break
@@ -95,15 +103,15 @@ def player_move(i):  # i = 플레이어 넘버
 # 0,0 0,1 0,2 0,3 0,4
 # 1,0 1,1 1,2 1,3 1,4
 
-graph_temp1 = [['a', 'a', 'b', 'b'],
-               ['a', 'a', 'e', 'f'],  # e 0,1
-               ['d', 'd', 'h', 'g'],  # g 2,3
-               ['d', 'd', 'c', 'c']]
-
-graph_temp2 = [[0, 0, 0, 0],
-               [0, 0, 0, 0],
-               [0, 0, 0, 0],
-               [0, 0, 0, 0]]
+# graph_temp1 = [['a', 'a', 'b', 'b'],
+#                ['a', 'a', 'e', 'f'],  # e 0,1
+#                ['d', 'd', 'h', 'g'],  # g 2,3
+#                ['d', 'd', 'c', 'c']]
+#
+# graph_temp2 = [[0, 0, 0, 0],
+#                [0, 0, 0, 0],
+#                [0, 0, 0, 0],
+#                [0, 0, 0, 0]]
 
 
 def rotate(start_x, start_y, last_x, last_y):
@@ -131,15 +139,16 @@ def rotate(start_x, start_y, last_x, last_y):
                 graph[i][j] = graph_temp[i - start_x][j - start_y] - 1
 
     for idx, player in enumerate(players):
-        #이러면 회전해야함
+        # 이러면 회전해야함
         if start_x <= player[0] <= last_x and start_y <= player[1] <= last_y:
-            players[idx][0], players[idx][1] = players[idx][1] - start_y + start_x, k - 1 - (players[idx][0] - start_x) + start_y
+            players[idx][0], players[idx][1] = players[idx][1] - start_y + start_x, k - 1 - (
+                        players[idx][0] - start_x) + start_y
+
 
 for game_iter in range(K):
-    # # 디버깅
+    # # # 디버깅
     # print(f'게임 진행 초 : {game_iter}')
     # Print(graph)
-    # Print(players)
     # print(player_exist)
 
     # 모든 참가자 동시 이동
@@ -148,14 +157,13 @@ for game_iter in range(K):
         if not player_exist[i]:
             continue
         player_move(i)
-    #디버깅
+    # # 디버깅
     # print('이동후')
     # Print(graph)
-    # Print(players)
 
     # 이동 이후 모든 플레이어가 다 나갔다면...?
     # 즉시 종료
-    if not number_of_players[0]:
+    if number_of_players[0] <= 0:
         break
 
     # 모든 플레이어가 이동을 마침...!
@@ -173,15 +181,51 @@ for game_iter in range(K):
         if next_distance > cur_distance:
             next_x, next_y = cur_x, cur_y
             next_distance = cur_distance
+            ####
+            start_x, last_x = min(next_x, exit_location[0]), max(next_x, exit_location[0])
+            start_y, last_y = min(next_y, exit_location[1]), max(next_y, exit_location[1])
+            if (last_x - start_x) == next_distance:
+                # x좌표는 놔두고 적절한 y좌표만 확인
+                while last_y - start_y != next_distance:
+                    if start_y > 1:
+                        start_y -= 1
+                    else:
+                        last_y += 1
+
+            elif (last_y - start_y) == next_distance:
+                # x좌표는 놔두고 적절한 y좌표만 확인
+                while last_x - start_x != next_distance:
+                    if start_x > 1:
+                        start_x -= 1
+                    else:
+                        last_x += 1
+                        #####
 
         elif next_distance == cur_distance:
-            if next_x > cur_x:
-                next_x, next_y = cur_x, cur_y
-                next_distance = cur_distance
-            elif next_x == cur_x:
-                if next_y > cur_y:
-                    next_x, next_y = cur_x, cur_y
-                    next_distance = cur_distance
+
+            cur_start_x, cur_last_x = min(cur_x, exit_location[0]), max(cur_x, exit_location[0])
+            cur_start_y, cur_last_y = min(cur_y, exit_location[1]), max(cur_y, exit_location[1])
+            if (cur_last_x - cur_start_x) == next_distance:
+                # x좌표는 놔두고 적절한 y좌표만 확인
+                while cur_last_y - cur_start_y != next_distance:
+                    if cur_start_y > 1:
+                        cur_start_y -= 1
+                    else:
+                        cur_last_y += 1
+
+            elif (cur_last_y - cur_start_y) == next_distance:
+                # x좌표는 놔두고 적절한 y좌표만 확인
+                while cur_last_x - cur_start_x != next_distance:
+                    if cur_start_x > 1:
+                        cur_start_x -= 1
+                    else:
+                        cur_last_x += 1
+            if cur_start_x < start_x:
+                start_x, start_y = cur_start_x, cur_start_y
+            elif cur_start_x == start_x:
+                if cur_start_y < start_y:
+                    start_x, start_y = cur_start_x, cur_start_y
+
     # 디버깅
     if next_x > N or next_y > N:
         print('정사각형 선택 오류 발생')
@@ -208,7 +252,7 @@ for game_iter in range(K):
                 last_x += 1
 
     rotate(start_x, start_y, last_x, last_y)
-    # #디버깅
+    # # #디버깅
     # print('회전후')
     # Print(graph)
     # Print(players)
